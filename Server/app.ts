fd5e44db6b0b70733ecf13626ccc73186b236514
raja-naveed <rajadahri11@gmail.com>
2025-09-10 16:39:23 +0500

    fix: update server URI and CORS origin to use production addresses

--- Server/app.ts
+++ Server/app.ts
@@ -26,7 +26,7 @@ app.use(cookieParser());
 
 // cors
 app.use(cors({ 
-    origin: ['http://localhost:3000', 'http://localhost:3001', 'http://209.74.89.249:3000' ], 
+    origin: ['http://localhost:3000', 'http://localhost:3001', 'http://209.74.89.249:3000', "http://app.0xmintyn.com" ], 
     credentials: true 
 }));
 
